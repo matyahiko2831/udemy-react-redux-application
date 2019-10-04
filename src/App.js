f055978e530902cb7111cfc1a2929f32cd83fb0b
kazuya.kondo <kazuya.kondo@adglobe.co.jp>
2019-10-04 17:15:07 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
 import React from 'react';
 
-// function App() {
-//   return (
-//     <React.Fragment>
-//       <label htmlFor="bar">bar</label>
-//       <input
-//         type="text"
-//         onChange={() => {
-//           console.log('I am clicked!');
-//         }}
-//       />
-//     </React.Fragment>
-//   );
-// }
-
 const App = () => {
+  const profile = [
+    {
+      name: 'Taro',
+      age: 10
+    },
+    {
+      name: 'Hanako',
+      age: 5
+    },
+    {
+      name: 'aaa'
+    }
+  ];
+
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
+      {profile.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
     </div>
   );
 };
 
-const Cat = () => {
-  return <div>Hi!</div>;
+const User = props => {
+  return (
+    <div>
+      Hi, I am {props.name}, and {props.age} years old!
+    </div>
+  );
+};
+
+User.defaultProps = {
+  age: 1
 };
 
 export default App;
